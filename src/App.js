1fefa24bd67bffae268f77480e0e1f73c2a0d146
madfarizz <ahmfariz24@gmail.com>
2023-07-07 09:52:23 +0700

    Added relative path to /v1 and built the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Home from './pages/Home';
 
 function App() {
-    return (
-        <div>
-            <Home />
-        </div>
-    );
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    navigate('/v1', { replace: true });
+  }, [navigate]);
+
+  return (
+    <Home />
+  );
 }
 
 export default App;
